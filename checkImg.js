b3db355a3130c6993881a1987bd63768da9a02ea
DominickCa2 <115185398+DominickCa2@users.noreply.github.com>
2024-09-11 14:33:25 -0700

    Update checkImg.js

--- checkImg.js
+++ checkImg.js
@@ -1,17 +1,21 @@
 import { View, Image, Button, StyleSheet } from 'react-native';
 import { useRouter } from 'expo-router';
 import { useImgStore } from "./store";
-import { useEffect } from 'react';
 
 export default function CheckImg() {
   const { meep, setMeep } = useImgStore();
   const router = useRouter();
 
+  function retry() {
+    setMeep(null);
+    router.push("./cameraThingy")
+  }
+
   return (
     <View style={styles.container}>
       <Image source={{ uri: meep }} style={styles.img}/>
       <View style={styles.options}>
-        <Button title="Retry" onPress={() => {router.push("./cameraThingy")}}/>
+        <Button title="Retry" onPress={retry}/>
         <Button title="Ok" onPress={() => {router.push("inBetweenScreen")}} />
       </View>
     </View>
